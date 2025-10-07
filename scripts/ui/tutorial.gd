f690a8e476e1b6e1d88e45ade015862c6ce9b721
Iqna <88360957+Ignosuke@users.noreply.github.com>
2025-10-06 22:00:29 -0300

    itch.io project setup 0.3

---
+++ scripts/ui/tutorial.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var dialog_label: Label = %Dialog
+@onready var continue_label: Label = %Continue
+
+const dialog_msgs = [
+	"Hello traveler, seems like we've got some company",
+	"These guys never let me enjoy myself",
+	"I could use some help, whatdya say?",
+	"Listen, I lend you my magic",
+	"Try casting this spell: FLAME"
+]
+
+func _ready() -> void:
+	pass
